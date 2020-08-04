dc4e33368540741a63b4d7411f71e686cb16ef05
AllanRaf <rafale.uk@googlemail.com>
2020-08-04 15:10:18 +0100

    fix url

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { ChatRoomPage } from "./rooms/ChatRoomPage";
 import { createBrowserHistory as history } from "history";
 import "./App.css";
 
-export const URL = "https://allanschat.herokuapp.com";
+export const url = "https://allanschat.herokuapp.com";
 
 function App() {
   return (
